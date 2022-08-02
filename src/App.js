340e442b8b31073b1c5931f010ae55cacd14c8c1
Ashutosh singh <kshatriya.aashutosh@gmail.com>
2022-08-03 00:11:48 +0530

    react-js tailwind initial

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css'
+function App() {
+  return (
+  <div className="h-screen text-3xl font-bold underline flex justify-center items-center">
+    <p>JRHM2</p>
+  </div>
+  );
+}
+
+export default App;
